23fd22f9b326da88118fce9778b94850c02b0ab1
adityayadav-codes <adityayadav2905@gmail.com>
2023-03-24 10:00:00 +0530

    ArrayList in java

--- JavaCollectionFramework/ArrList.java
+++ JavaCollectionFramework/ArrList.java
@@ -46,23 +46,23 @@ public class ArrList {
 // 	•	Add elements
 // 	•	Print using loop
 
-// public class ArrList {
+public class ArrList {
 
-//     public static void main(String [] args) {
+    public static void main(String [] args) {
 
-//         ArrayList <Integer> list = new ArrayList<>();
+        ArrayList <Integer> list = new ArrayList<>();
 
-//         list.add(10);
-//         list.add(20);
-//         list.add(30);
+        list.add(10);
+        list.add(20);
+        list.add(30);
 
-//         // loop
-//         for(int i =0; i < list.size(); i++) {
+        // loop
+        for(int i =0; i < list.size(); i++) {
 
-//             System.out.println(list.get(i));
-//         }
-//     }
-// }
+            System.out.println(list.get(i));
+        }
+    }
+}
 
 /*
     2️⃣ Create LinkedList
