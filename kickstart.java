f46751a176ea4f2dd011cfb63fafc805f6787c87
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-13 23:12:07 +0530

    traversing 2d array

--- kickstart.java
+++ kickstart.java
@@ -154,3 +154,18 @@
 // }
 
 // 2-D array
+public class kickstart {
+
+	public static void main (String args []) {
+
+		int [] [] arr = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+		System.out.println("2-D array is: ");
+		for(int i =0; i < arr.length; i++) {
+			for(int j =0; j <  arr [i].length; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+	}
+}
+}
