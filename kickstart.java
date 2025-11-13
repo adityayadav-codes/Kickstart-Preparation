5ba70564b95bc0200fe862ecd110badc00be9b92
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-13 23:07:05 +0530

    now i start 2-D array

--- kickstart.java
+++ kickstart.java
@@ -118,37 +118,39 @@
 //     }
 // }
 
-//Insertion sort
-import java.util.Scanner;
-public class kickstart {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of elements:");
-		int n = sc.nextInt();
+// //Insertion sort
+// import java.util.Scanner;
+// public class kickstart {
+// 	public static void main(String[] args) {
+// 		Scanner sc = new Scanner(System.in);
+// 		System.out.println("Enter the number of elements:");
+// 		int n = sc.nextInt();
 
-		int[] arr = new int[n];
-		System.out.println("Enter " + n + " elements:");
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
+// 		int[] arr = new int[n];
+// 		System.out.println("Enter " + n + " elements:");
+// 		for (int i = 0; i < n; i++) {
+// 			arr[i] = sc.nextInt();
+// 		}
 
-		// Insertion sort logic
-		for (int i = 1; i < n; i++) {
-			int key = arr[i];
-			int j = i - 1;
+// 		// Insertion sort logic
+// 		for (int i = 1; i < n; i++) {
+// 			int key = arr[i];
+// 			int j = i - 1;
 
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j = j - 1;
-			}
-			arr[j + 1] = key;
-		}
+// 			while (j >= 0 && arr[j] > key) {
+// 				arr[j + 1] = arr[j];
+// 				j = j - 1;
+// 			}
+// 			arr[j + 1] = key;
+// 		}
 
-		System.out.println("Sorted array:");
-		for (int i = 0; i < n; i++) {
-			System.out.print(arr[i] + " ");
-		}
+// 		System.out.println("Sorted array:");
+// 		for (int i = 0; i < n; i++) {
+// 			System.out.print(arr[i] + " ");
+// 		}
 
-		sc.close();
-	}
-}
+// 		sc.close();
+// 	}
+// }
+
+// 2-D array
