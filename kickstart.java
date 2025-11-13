925bdf46de7ce8ad24eb532078a21a7bc252862f
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-13 23:17:30 +0530

    sum of the 2-D matrix

--- kickstart.java
+++ kickstart.java
@@ -158,14 +158,24 @@ public class kickstart {
 
 	public static void main (String args []) {
 
+		int sum =0;
 		int [] [] arr = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
 
 		System.out.println("2-D array is: ");
 		for(int i =0; i < arr.length; i++) {
 			for(int j =0; j <  arr [i].length; j++){
 				System.out.print(arr[i][j] + " ");
-			}
+			}	
 			System.out.println();
 	}
+	for(int i =0 ; i < arr.length; i++) {
+
+		for ( int j =0; j < arr [i].length; j++) {
+
+			sum += arr [i][j];
+
+		}	
+	}
+	System.out.println("Sum of all elements in 2-D array is: " + sum);
 }
 }
