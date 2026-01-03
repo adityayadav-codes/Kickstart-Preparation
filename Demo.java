a4def993651637d7d68c52cd7038185aaa002648
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-03 23:11:13 +0530

    print my details

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public class Demo {
+
     public static void main(String [] args){
         System.out.println("Aditya Yadav");
+        System.out.println("Mehnagar, Azamgarh");
+        System.out.println("727984934");
     }
 }
