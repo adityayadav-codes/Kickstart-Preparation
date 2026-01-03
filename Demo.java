8bd897da370822e0c51fd3239688522372953aeb
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-03 23:16:50 +0530

    1st program

--- Demo.java
+++ Demo.java
@@ -1,8 +1,7 @@
 public class Demo {
     static int a = 10;
-    static int b = 20;
-    
-        public static void main(String [] args ) {
-            System.out.println("Sum is : " +(a + b));
-        }
+    static int  b = 20;
+     public static void main(String [] args ) {
+        System.out.println("Multpliction is : " +(a * b));
+     }
 }
