e0b604adb33806224c7891b85a67d418936cabf7
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-04 18:30:27 +0530

    A program to demonstrate adult or not

--- Demo.java
+++ Demo.java
@@ -1,8 +1,7 @@
-public class Demo {
-    static int num = 12;
-    public static void main (String [] args) {
-        if(num > 10) {
-            System.out.println("Number is greater than 10");
-        }
+public class Demo{
+    static int age = 25;
+    public static void main(String [] args ) {
+        if (age > 18)
+        System.out.println("You r adult");
     }
 }
