30daf340546e964b46fe69981dbdb34bc42cad5e
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-04 18:27:37 +0530

    A simple progrem to demonstrate number is greater than 10 or not

--- Demo.java
+++ Demo.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
 public class Demo {
-    public static void main(String [] args ){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the age :");
-        int age = sc.nextInt();
-        System.out.println("Your age is : " + age);
-        sc.getClass();
+    static int num = 12;
+    public static void main (String [] args) {
+        if(num > 10) {
+            System.out.println("Number is greater than 10");
+        }
     }
 }
-// logic - logic is in programming a set of instructions to perform a specific task or solve a problem. It involves using control structures, data manipulation, and algorithms to achieve the desired outcome. 
