74de23c97920a0c06a1103d7574ce65d94942c05
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-05 21:34:18 +0530

    A program to demonstrate grade

--- Demo.java
+++ Demo.java
@@ -1,12 +1,19 @@
 public class Demo {
-    public static void main(String [] args) {
-        int a = 5;
-        int b = 10;
+    public static void main (String[] args) {
+        int marks = 85;
 
-        if(a > b) {
-            System.out.println("A is greater than b");
-        } else {
-            System.out.println("B is greater than or equal to A");
-        }
+         if(marks >=80) {
+            System.out.println("A+ Grade");
+         } else if (marks >= 70) {
+            System.out.println("A grade ");
+         } else if (marks >= 60) {
+            System.out.println("B grade");
+         } else if(marks>=50) {
+            System.out.println("C grade");
+         } else if(marks >=33) {
+            System.out.println("D grade");
+         } else {
+            System.out.println("Fail");
+         }
     }
 }
