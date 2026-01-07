42a7d9a35aef92f2d530d6061fc35ae8d3699d76
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-07 20:23:35 +0530

    A Program to demonstrate Print table of 5

--- Demo1.java
+++ Demo1.java
@@ -1,11 +1,15 @@
 public class Demo1 {
-
-    public static void main (String [] args ) {
-        int num = 20;
-        for(int i = 1; i <=num; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i + " is even number");
+    
+        public static void main (String [] arrgs) {
+            int num = 5;
+            System.out.println("table of 5: ");
+            for(int i = 1; i <=10; i++) {
+                 
+                System.out.println(num * i);
             }
         }
-    }
+
+
+
+
 }
