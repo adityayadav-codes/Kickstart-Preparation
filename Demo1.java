b46f3d4881a778f773314a8e787dd825c4a0d921
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-07 20:27:05 +0530

    A program to demonstrate sum of first 10 number

--- Demo1.java
+++ Demo1.java
@@ -1,15 +1,10 @@
 public class Demo1 {
-    
-        public static void main (String [] arrgs) {
-            int num = 5;
-            System.out.println("table of 5: ");
-            for(int i = 1; i <=10; i++) {
-                 
-                System.out.println(num * i);
-            }
+
+    public static void main(String [] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++ ) {
+            sum += i;
+             System.out.println("Sum after adding " + i + " is: " + sum);
         }
-
-
-
-
+    }
 }
