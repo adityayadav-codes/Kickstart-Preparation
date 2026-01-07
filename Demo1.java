13a08f91ef674002eb91eabb924ceb6ea50ab29f
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-07 20:16:23 +0530

    A Program to deomonstrate Prints ever numbers 1 to 10

--- Demo1.java
+++ Demo1.java
@@ -1,10 +1,11 @@
-
 public class Demo1 {
-    public static void main (String [] args) {
-        int num = 10;
 
-        for(int i = 1; i <= num; i++) {
-            System.out.print(i + " ");
+    public static void main (String [] args ) {
+        int num = 20;
+        for(int i = 1; i <=num; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even number");
+            }
         }
     }
 }
