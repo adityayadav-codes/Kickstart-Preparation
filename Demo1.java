7a89789ea8417eab33fc60f2298146099e86c890
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-08 14:22:39 +0530

    print stars in a row

--- Demo1.java
+++ Demo1.java
@@ -1,13 +1,10 @@
 public class Demo1 {
 
-     public static void main(String [] args ) {
-
-            for(int i = 1; i <=5;i++) {
-
-                for(int j =1; j <= 1; j++) {
-
-                    System.out.println("* ");
-                }
-            }
-     }
+    public static void main(String [] args) {
+        for(int i =1; i <=1;i++) {
+            for(int j = 1; j <=5;j++) {
+                System.out.print("* ");
+            }            System.out.println();
+        }
+    }
 }
