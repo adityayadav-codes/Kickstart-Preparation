2da5d69728412e81fcfbbe709dfb4482af5da665
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-10 19:36:44 +0530

    Right-Angle Triangle print

--- Demo1.java
+++ Demo1.java
@@ -1,10 +1,16 @@
-public class Demo1 {
-
+/*
+*
+**
+***
+****
+ */
+public class Demo1{
     public static void main(String [] args) {
-        for(int i =1; i <=1;i++) {
-            for(int j = 1; j <=5;j++) {
+        for(int i =1; i <= 4; i++) {
+            for(int j = 1; j <= i; j++) {
                 System.out.print("* ");
-            }            System.out.println();
-        }
+            }
+            System.out.println();
     }
+}
 }
