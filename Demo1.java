9f16faf8e3591bd52b933c21edc3a6654f5cb8cd
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-15 13:48:19 +0530

    increase readability of code

--- Demo1.java
+++ Demo1.java
@@ -1,20 +1,18 @@
 public class Demo1 {
+
     public static void main(String[] args) {
-
-        int[] arr = {10, 25, 30, 45, 50};
-        int key = 45;
+        int [] arr = {10, 20, 30, 40, 50};
+        int key = 30;
         boolean found = false;
-
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 0; i <arr.length; i++) {
             if(arr[i] == key) {
                 System.out.println("Element found at index: " + i);
                 found = true;
                 break;
             }
         }
-
         if(!found) {
-            System.out.println("Element not found");
+            System.out.println("Element not found in the array.");
         }
     }
 }
