fd4382399a5f4105ee3dc6c0551c2ae0f9236344
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-15 12:46:55 +0530

    a program to demonstrate prints even number in an array

--- Demo1.java
+++ Demo1.java
@@ -1,14 +1,14 @@
 public class Demo1 {
-
-    public static void main(String [] args) {
-        int [] arr = {10, 70, 30, 40, 50};
-        int maxNumber = arr[0];
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] >maxNumber) {
-                maxNumber = arr[i];
-               
-            }
-        }
-        System.out.println("The maximum number in the array is: " + maxNumber);
-    }
+	public static void main (String [] args) {
+	
+		int [] arr = {2,4,3,5,6,8,10,13};
+        int count = 0;		
+			for(int i = 0; i < arr.length; i++) {
+			
+				if(arr[i] % 2 == 0) {
+					count ++;
+			}
+		}
+        System.out.println("Number of even elements in the array: " + count);
+	}
 }
