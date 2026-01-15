7a939de7482afea9d44f46d710290a984c1fd362
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-15 12:56:32 +0530

    A Program to demonstrate count odd numbers in an array

--- Demo1.java
+++ Demo1.java
@@ -1,14 +1,14 @@
 public class Demo1 {
-	public static void main (String [] args) {
-	
-		int [] arr = {2,4,3,5,6,8,10,13};
-        int count = 0;		
-			for(int i = 0; i < arr.length; i++) {
-			
-				if(arr[i] % 2 == 0) {
-					count ++;
-			}
+	public static void main(String [] args) {
+		
+		int [] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+		int count = 0;
+		
+		for(int i =0; i < arr.length; i++) {
+            if(arr[i] % 2 !=0){
+                count++;
+            }
 		}
-        System.out.println("Number of even elements in the array: " + count);
+        System.out.println("Total odd numbers in the array: " + count);
 	}
 }
