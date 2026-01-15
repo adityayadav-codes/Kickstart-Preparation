7851984797eb0727c3d43f9d579b0d6a63dc6325
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-15 12:32:48 +0530

    A program to demonstrate prints maximum number in an array

--- Demo1.java
+++ Demo1.java
@@ -1,21 +1,14 @@
 public class Demo1 {
-    static int arr[] = {34, -2, 45, 29, 8, -10, 0, 99, 23};
-    static int maxNumber = arr[0];
-    static int minNumber = arr[0];
+
     public static void main(String [] args) {
-        
-        for(int i = 1; i <arr.length; i++) {
-            if(arr[i] > maxNumber) {
+        int [] arr = {10, 70, 30, 40, 50};
+        int maxNumber = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] >maxNumber) {
                 maxNumber = arr[i];
+               
             }
-          else if (arr[i]< minNumber) {
-                minNumber = arr[i];
-          }  
-          else {
-              continue;
-          }
         }
-        System.out.println("Maximum number in the array is : "+maxNumber);
-        System.out.println("Minimum number in the array is : "+minNumber);
+        System.out.println("The maximum number in the array is: " + maxNumber);
     }
 }
