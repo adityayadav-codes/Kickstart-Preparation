a1b5414a7c50784859d4abec6ca4d797c92d34eb
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-19 21:52:56 +0530

    add code for count constant in String

--- String in java/javaString.java
+++ String in java/javaString.java
@@ -1,6 +1,23 @@
 public class javaString {
 
+        void countConsonants() {
+
+            String str = "java";
+            int count = 0;
+            for(int i =0; i <str.length(); i++) {
+
+                if(str.charAt(i)!='a' && str.charAt(i) != 'e' && str.charAt(i) != 'i' && str.charAt(i) != 'o' && str.charAt(i)!='u') {
+                    
+                   System.out.println("Consonant: " + str.charAt(i) + " at index " + i);
+                    count++;
+                } 
+               
+            }
+            System.out.println("Total consonants: " + count);
+        }
     public static void main(String [] args) {
+        javaString js = new javaString();
+        js.countConsonants();
         String str = "java";
        int  count =0;
 
@@ -8,12 +25,12 @@ public class javaString {
 
             if(str.charAt(i)=='a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i)=='u') {
                 
-                System.out.println("Vowel: " + str.charAt(i) + " at index " + i);
+             //   System.out.println("Vowel: " + str.charAt(i) + " at index " + i);
                 count++;
             } 
            
         }
-         System.out.println("Total vowels: " + count);
+        // System.out.println("Total vowels: " + count);
     }
 
  }
