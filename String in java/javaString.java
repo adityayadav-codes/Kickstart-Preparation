74ca65d65427222484403cc0f43d97bf940d3187
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-19 21:59:54 +0530

    update in code but logic same

--- String in java/javaString.java
+++ String in java/javaString.java
@@ -1,20 +1,18 @@
 public class javaString {
 
-        void countConsonants() {
+       void countConsonants(){
+         String str = "java";
+        int count =0;
 
-            String str = "java";
-            int count = 0;
-            for(int i =0; i <str.length(); i++) {
-
-                if(str.charAt(i)!='a' && str.charAt(i) != 'e' && str.charAt(i) != 'i' && str.charAt(i) != 'o' && str.charAt(i)!='u') {
-                    
-                   System.out.println("Consonant: " + str.charAt(i) + " at index " + i);
+            for(int i =0; i < str.length(); i++) {
+                if(str.charAt(i)!= 'a' && str.charAt(i) !='e' && str.charAt(i) != 'i' && str.charAt(i)!= 'o' && str.charAt(i)!='u') {
+                    System.out.println("Consonant: " +str.charAt(i) + " at index"+i);
                     count++;
-                } 
+                }
                
             }
-            System.out.println("Total consonants: " + count);
-        }
+             System.out.println("Total Consonants:"+count);
+       }
     public static void main(String [] args) {
         javaString js = new javaString();
         js.countConsonants();
