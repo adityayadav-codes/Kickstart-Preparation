e3835f38ba8d0972f33643e2db9e228bd9b8b7fc
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-19 21:01:24 +0530

    A Program to demonstrate returns the character at the specific index in the array

--- String in java/javaString.java
+++ String in java/javaString.java
@@ -1,7 +1,9 @@
 public class javaString {
     public static void main(String [] args) {
 
-        String s = "Java";
-        System.out.println(s.length()); // length of the string
+        // String s = "Java";
+        // System.out.println(s.length()); // length of the string
+        String s = "JAVA";
+        System.out.println(s.charAt(2));
     }
 }
