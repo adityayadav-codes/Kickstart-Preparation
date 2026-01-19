080877655d5acd4e8d519f6e82da8a3c69624dd9
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-19 21:07:16 +0530

    A program to demonstrate return legth of String

---
+++ String in java/javaString.java
@@ -0,0 +1,7 @@
+public class javaString {
+
+    public static void main(String [] args ) {
+        String str1 = "Java is my life & I love Java";
+        System.out.println(str1.length());
+    }
+}
