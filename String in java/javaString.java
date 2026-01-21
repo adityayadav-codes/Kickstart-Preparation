a644d2e7f078894edf4f2c54d2935b44fcdae2b7
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-21 21:37:26 +0530

    update javaString file

--- String in java/javaString.java
+++ String in java/javaString.java
@@ -19,17 +19,32 @@ public class javaString {
                 System.out.println(str.charAt(i));
             }
         }
-        void countVowels() {
-            String sr = "Hello java Programming";
-            int count =0;
-            for(int i =0; i < sr.length(); i++) {
-                if(sr.charAt(i)=='a'|| sr.charAt(i)=='e'|| sr.charAt(i)=='i'|| sr.charAt(i)=='o'|| sr.charAt(i)=='u') {
-                    System.out.println("Vowel: " + sr.charAt(i) + " at index " + i);
-                    count++;
-                }
-            }
-            System.out.println("Total Vowels: " + count);
-        }
+        // void countVowels() {
+        //     // String sr = "Hello java Programming";
+        //     // int count =0;
+        //     // for(int i =0; i < sr.length(); i++) {
+        //     //     if(sr.charAt(i)=='a'|| sr.charAt(i)=='e'|| sr.charAt(i)=='i'|| sr.charAt(i)=='o'|| sr.charAt(i)=='u') {
+        //     //         System.out.println("Vowel: " + sr.charAt(i) + " at index " + i);
+        //     //         count++;
+        //     //     }
+        //     // }
+        //     // System.out.println("Total Vowels: " + count);
+        // }
+        void  countVowels() {
+
+	String str = "Java";
+	int count =0;
+	for(int i = 0; i < str.length(); i++) {
+
+	if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+
+		System.out.println("vowels : " + str.charAt(i) + "at index "+i);
+		count ++;
+	}
+	System.out.println("Total Vowels "+count);
+}
+
+
         public static void main(String [] args) {
         javaString js = new javaString();
        // js.countConsonants();
