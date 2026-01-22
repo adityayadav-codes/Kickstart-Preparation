d310606ffdcb02485e9516bdc77c501dabefadea
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-22 23:55:30 +0530

    method 2 succefully

--- String in java/javaString.java
+++ String in java/javaString.java
@@ -157,7 +157,7 @@ public class javaString {
                 result = result + str.charAt(i);
             }
         }
-        System.out.println("result");
+        System.out.println(result);
     }
     public static void main(String[] args) {
         javaString js = new javaString();
