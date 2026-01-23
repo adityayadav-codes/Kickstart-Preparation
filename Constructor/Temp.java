76bb00d66a4bc8cd0adfa3a5f2b48e822135ddeb
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-23 22:53:15 +0530

    User-defined current opject(explicit references

--- Constructor/Temp.java
+++ Constructor/Temp.java
@@ -37,24 +37,47 @@
 
 // this keyword==========================
 
+// public class Temp {
+// 	String  name ;
+// 	int age;
+	
+// 		Temp(String name, int age) {
+// 		// this keyword
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// 		void display() {
+		
+// 			System.out.println("Name : "+name);
+// 			System.out.println("Age  : "+age);
+// 	}
+	
+// 	public static void main(String [] args) {
+	
+// 		Temp t1 = new Temp("Aditya",20);
+// 		t1.display();
+// 	}
+// }
+
 public class Temp {
-	String  name ;
+	
+	String name;
 	int age;
 	
 		Temp(String name, int age) {
-		// this keyword
-		this.name = name;
-		this.age = age;
+            Temp obj = this;
+			obj.name = name;
+			obj.age = age;
 	}
+		
 		void display() {
 		
-			System.out.println("Name : "+name);
-			System.out.println("Age  : "+age);
+		System.out.println("Name : "+name);
+		System.out.println("Age    :"+age);
 	}
-	
 	public static void main(String [] args) {
-	
-		Temp t1 = new Temp("Aditya",20);
-		t1.display();
+		
+		Temp t1 = new Temp("Aditya", 20);
+        t1.display();
 	}
 }
