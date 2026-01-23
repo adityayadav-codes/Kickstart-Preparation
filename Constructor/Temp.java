8677270e01a0f9d898f1d1b64120b08c7af5d73f
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-23 22:32:34 +0530

    return null and 0

--- Constructor/Temp.java
+++ Constructor/Temp.java
@@ -16,21 +16,45 @@
 		
 // 	}
 // }
+// public class Temp {
+		
+// 	int a;
+// 	int b;
+// 		// Parameterized Constructor:
+// 	Temp(int x, int y) {
+		
+// 		a = x; 
+// 		b = y;
+		
+// 			System.out.println("a = "+  " "+ "b = "+b);
+// 			System.out.println("Sum = "+ (a+b));
+// 	}
+// 	public static void main (String [] args) {
+// 		System.out.println("Parametirezed Constructor: ");
+// 		Temp  t1 = new Temp(10, 20);
+// 	}
+// }
+
+// this keyword==========================
+
 public class Temp {
+	String  name ;
+	int age;
+	
+		Temp(String name, int age) {
 		
-	int a;
-	int b;
-		// Parameterized Constructor:
-	Temp(int x, int y) {
-		
-		a = x; 
-		b = y;
-		
-			System.out.println("a = "+  " "+ "b = "+b);
-			System.out.println("Sum = "+ (a+b));
+		name = name;
+		age = age;
 	}
-	public static void main (String [] args) {
-		System.out.println("Parametirezed Constructor: ");
-		Temp  t1 = new Temp(10, 20);
+		void display() {
+		
+			System.out.println("Name : "+name);
+			System.out.println("Age  : "+age);
+	}
+	
+	public static void main(String [] args) {
+	
+		Temp t1 = new Temp("Aditya",20);
+		t1.display();
 	}
 }
