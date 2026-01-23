3501e81693bedd6e371830b72ad833c1dcc66190
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-23 22:17:05 +0530

    Parameterized Constructor

--- Constructor/Temp.java
+++ Constructor/Temp.java
@@ -1,20 +1,36 @@
-package Constructor;
-
-public class Temp {
+// public class Temp {
 	
-	int a ;
-	int b;
-		Temp() {
+// 	int a ;
+// 	int b;
+// 		Temp() {
 			
-			a = 10;
-			b = 20;
-		System.out.println("a = " +a + "  " + "b = "+b);
-		System.out.println("Sum :  " +(a+b));
-	}
+// 			a = 10;
+// 			b = 20;
+// 		System.out.println("a = " +a + "  " + "b = "+b);
+// 		System.out.println("Sum :  " +(a+b));
+// 	}
 	
-	public static void main(String [] args ) {
+// 	public static void main(String [] args ) {
 		
-		Temp t1 = new Temp();
+// 		Temp t1 = new Temp();
 		
+// 	}
+// }
+public class Temp {
+		
+	int a;
+	int b;
+		// Parameterized Constructor:
+	Temp(int x, int y) {
+		
+		a = x; 
+		b = y;
+		
+			System.out.println("a = "+  " "+ "b = "+b);
+			System.out.println("Sum = "+ (a+b));
+	}
+	public static void main (String [] args) {
+		System.out.println("Parametirezed Constructor: ");
+		Temp  t1 = new Temp(10, 20);
 	}
 }
