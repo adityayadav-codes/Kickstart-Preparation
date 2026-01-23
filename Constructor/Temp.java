7a388c0c46df718883666d2cba1514df2bc334d7
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-23 22:07:27 +0530

    Defualt construcotr

---
+++ Constructor/Temp.java
@@ -0,0 +1,18 @@
+ public class Temp {
+	
+	int a ;
+	int b;
+		Temp() {
+			
+			a = 10;
+			b = 20;
+		System.out.println("a = " +a + "  " + "b = "+b);
+		System.out.println("Sum :  " +(a+b));
+	}
+	
+	public static void main(String [] args ) {
+		
+		Temp t1 = new Temp();
+		
+	}
+}
