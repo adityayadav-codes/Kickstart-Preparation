5750b2eb38ae5dca87447ee539891b105bcfd212
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-27 22:24:43 +0530

    update ocde

--- Constructor/Temp.java
+++ Constructor/Temp.java
@@ -1,87 +1,87 @@
-// public class Temp {
+ class Temp {
 	
-// 	int a ;
-// 	int b;
-// 		Temp() {
+	int a ;
+	int b;
+		Temp() {
 			
-// 			a = 10;
-// 			b = 20;
-// 		System.out.println("a = " +a + "  " + "b = "+b);
-// 		System.out.println("Sum :  " +(a+b));
-// 	}
+			a = 10;
+			b = 20;
+		System.out.println("a = " +a + "  " + "b = "+b);
+		System.out.println("Sum :  " +(a+b));
+	}
 	
-// 	public static void main(String [] args ) {
+	 static void main(String [] args ) {
 		
-// 		Temp t1 = new Temp();
+		Temp t1 = new Temp();
 		
-// 	}
-// }
-// public class Temp {
+	}
+}
+ class Temp {
 		
-// 	int a;
-// 	int b;
-// 		// Parameterized Constructor:
-// 	Temp(int x, int y) {
+	int a;
+	int b;
+		// Parameterized Constructor:
+	Temp(int x, int y) {
 		
-// 		a = x; 
-// 		b = y;
+		a = x; 
+		b = y;
 		
-// 			System.out.println("a = "+  " "+ "b = "+b);
-// 			System.out.println("Sum = "+ (a+b));
-// 	}
-// 	public static void main (String [] args) {
-// 		System.out.println("Parametirezed Constructor: ");
-// 		Temp  t1 = new Temp(10, 20);
-// 	}
-// }
+			System.out.println("a = "+  " "+ "b = "+b);
+			System.out.println("Sum = "+ (a+b));
+	}
+	 static void main (String [] args) {
+		System.out.println("Parametirezed Constructor: ");
+		Temp  t1 = new Temp(10, 20);
+	}
+}
 
-// this keyword==========================
+//this keyword==========================
 
-// public class Temp {
-// 	String  name ;
-// 	int age;
+ class Temp {
+	String  name ;
+	int age;
 	
-// 		Temp(String name, int age) {
-// 		// this keyword
-// 		this.name = name;
-// 		this.age = age;
-// 	}
-// 		void display() {
+		Temp(String name, int age) {
+		// this keyword
+		this.name = name;
+		this.age = age;
+	}
+		void display() {
 		
-// 			System.out.println("Name : "+name);
-// 			System.out.println("Age  : "+age);
-// 	}
+			System.out.println("Name : "+name);
+			System.out.println("Age  : "+age);
+	}
 	
-// 	public static void main(String [] args) {
+	 static void main(String [] args) {
 	
-// 		Temp t1 = new Temp("Aditya",20);
-// 		t1.display();
-// 	}
-// }
+		Temp t1 = new Temp("Aditya",20);
+		t1.display();
+	}
+}
 
-// public class Temp {
+ class Temp {
 	
-// 	String name;
-// 	int age;
+	String name;
+	int age;
 	
-// 		Temp(String name, int age) {
-//             Temp obj = this;
-// 			obj.name = name;
-// 			obj.age = age;
-// 	}
+		Temp(String name, int age) {
+            Temp obj = this;
+			obj.name = name;
+			obj.age = age;
+	}
 		
-// 		void display() {
+		void display() {
 		
-// 		System.out.println("Name : "+name);
-// 		System.out.println("Age    :"+age);
-// 	}
-// 	public static void main(String [] args) {
+		System.out.println("Name : "+name);
+		System.out.println("Age    :"+age);
+	}
+	 static void main(String [] args) {
 		
-// 		Temp t1 = new Temp("Aditya", 20);
-//         t1.display();
-// 	}
-// }
-public class Temp {
+		Temp t1 = new Temp("Aditya", 20);
+        t1.display();
+	}
+}
+ public class Temp {
 		
 	String name;
 	int age;
@@ -99,7 +99,7 @@ public class Temp {
 			System.out.println("Age.   :"+age);
 	}
 	
-	public static void main(String [] args) {
+	 static void main(String [] args) {
 		
 		Temp stu1 = new Temp("Aditya Yadav", 20, 72);
         stu1.display();
