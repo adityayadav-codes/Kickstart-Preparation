8785a5ab1102811bd677663d8ac6add928052390
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-02 19:53:29 +0530

    rename file name & add a real life example

---
+++ Encapsulation/Main.java
@@ -0,0 +1,26 @@
+// public class Student {
+// 	private int age;
+	
+// 		public void setAge(int age) {
+		
+// 			if(age > 0 ) {
+			
+// 				this.age = age;
+// 		}
+// 	}
+	
+// 		public int getAge() {
+		
+// 			return age;
+// 	}	
+
+// 	public static void main(String [] args) {
+		
+// 		Student s1 = new Student();
+// 		s1.setAge(20);
+// 		System.out.println("Age : "+s1.getAge());
+// 	}
+// }
+
+
+// ========== Real-life Example for Encapsulation & Data Hiding ===========================
