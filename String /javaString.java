defbba317435094204b6c1a3b7ff1a810bd54690
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-02 22:37:44 +0530

    console string through charAt()

--- String /javaString.java
+++ String /javaString.java
@@ -188,7 +188,7 @@ public class javaString {
 
         String str = "hii aadi";
         for(int i = str.length()-1; i > 0; i--) {
-            
+            System.out.println(str.charAt(i));
         }
    }
    public static void main(String[] args) {
