2c664a04e258b486700299815ebcd24d7b868e8f
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-04 20:12:56 +0530

    remove space for reverse string

--- String /CheckPalindrome.java
+++ String /CheckPalindrome.java
@@ -2,7 +2,7 @@ public  class CheckPalindrome {
     public static void main(String[] args) {
         
         String str = "radar";
-        String rev = " ";
+        String rev = "";
 
         for(int i = str.length()-1; i >= 0; i--) {
 
