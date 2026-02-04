185d640a610937c478ebad60e74772c0664dabf0
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-04 20:09:31 +0530

    value holds in rev variable through + operator

--- String /CheckPalindrome.java
+++ String /CheckPalindrome.java
@@ -2,11 +2,12 @@ public  class CheckPalindrome {
     public static void main(String[] args) {
         
         String str = "radar";
-        String reg = " ";
+        String rev = " ";
 
         for(int i = str.length()-1; i >= 0; i--) {
-            
+
+            rev = rev + str.charAt(i);
         }
+    
     }
-            
 }
