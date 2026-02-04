d94322bd6a104ea2ddfdef05135a352951801e94
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-04 20:05:45 +0530

    declare variables

--- String /CheckPalindrome.java
+++ String /CheckPalindrome.java
@@ -1,3 +1,10 @@
 public  class CheckPalindrome {
-    
+    public static void main(String[] args) {
+        
+        String str = "radar";
+        String reg = " ";
+        
+
+    }
+            
 }
