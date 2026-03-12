42c15826779f0e459a4626aebfedf4bdd46e3043
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-12 18:20:13 +0530

    Calculate Example via through Method Overloading in Polymorphism

--- Polymorphism/MethodOverloading/Demo.java
+++ Polymorphism/MethodOverloading/Demo.java
@@ -1,5 +1,21 @@
-package Polymorphism.MethodOverloading;
-
+class Calculate {
+    int  add(int a, int b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    double add (int a, int b, int c, int d) {
+        
+        return a + b + c + d;
+    }
+}
 public class Demo {
-    
+    public static void main(String [] args) {
+
+        Calculate obj = new Calculate();
+       System.out.println(obj.add(10, 20));
+       System.out.println(obj.add(10, 20, 30));
+       System.out.println(obj.add(10, 20, 30, 40));
+    }
 }
