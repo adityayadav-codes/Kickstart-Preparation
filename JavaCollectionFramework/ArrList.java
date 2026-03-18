763fef53570c258c9357aae584cab321371547de
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-18 19:56:29 +0530

    add() method in ArrayList

--- JavaCollectionFramework/ArrList.java
+++ JavaCollectionFramework/ArrList.java
@@ -1,3 +1,25 @@
+/*
+
+Create an ArrayList of student names:
+	•	Add 5 names
+	•	Print all names
+	•	Remove one name
+	•	Print updated list
+
+*/
+import java.util.*;
+
 public class ArrList {
-    
+
+    public static void main(String [] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Brijnath Yadav");
+        list.add("Vidya Devi"); 
+        list.add("Suman Yadav");
+        list.add("Soni Yadav");
+        list.add("Golu Yadav");
+        list.add("Babu Yadav");
+    }
 }
