93fcc3e3f4127edfb8866d44a55a62c0d8495438
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-19 13:06:45 +0530

    Creates ArayList of Integer 🟢Add element & 🟢 Print using Loop

--- JavaCollectionFramework/ArrList.java
+++ JavaCollectionFramework/ArrList.java
@@ -10,34 +10,56 @@ Create an ArrayList of student names:
 */
 
 
+// public class ArrList {
+
+//     public static void main(String [] args) {
+
+//         ArrayList<String> list = new ArrayList<>();
+
+//         // add()=================================
+//         list.add("Aditya Yadav");
+//         list.add("Akash Yadav"); 
+//         list.add("Anil Singh");
+//         list.add("Chandan Thakur");
+//         list.add("Abhishek Yadav");
+//         list.add("Daksh Chauhan");
+//         list.add("Deepak Kumar");
+
+//         // get()=================================
+//             System.out.println(list.get(0));
+//             System.out.println(list.get(1));
+//             System.out.println(list.get(2));
+//             System.out.println(list.get(3));
+//             System.out.println(list.get(4));
+//             System.out.println(list.get(5)); 
+
+//         // remove ==================================
+//         list.remove(2);
+//         System.out.println(list);     
+
+//     // size () =========================================
+//     System.out.println(list.size());
+//     }
+// }
+
+// 1️⃣ Create ArrayList of integers
+	// •	Add elements
+	// •	Print using loop
+
 public class ArrList {
 
     public static void main(String [] args) {
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList <Integer> list = new ArrayList<>();
 
-        // add()=================================
-        list.add("Aditya Yadav");
-        list.add("Akash Yadav"); 
-        list.add("Anil Singh");
-        list.add("Chandan Thakur");
-        list.add("Abhishek Yadav");
-        list.add("Daksh Chauhan");
-        list.add("Deepak Kumar");
+        list.add(10);
+        list.add(20);
+        list.add(30);
 
-        // get()=================================
-            System.out.println(list.get(0));
-            System.out.println(list.get(1));
-            System.out.println(list.get(2));
-            System.out.println(list.get(3));
-            System.out.println(list.get(4));
-            System.out.println(list.get(5)); 
+        // loop
+        for(int i =0; i < list.size(); i++) {
 
-        // remove ==================================
-        list.remove(2);
-        System.out.println(list);     
-
-    // size () =========================================
-    System.out.println(list.size());
+            System.out.println(list.get(i));
+        }
     }
 }
