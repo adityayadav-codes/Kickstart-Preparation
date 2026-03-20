1e1bed44f74724d14c55119eb7307f002ff42f0d
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-20 12:05:08 +0530

    Prime number

--- Revision/Rev.java
+++ Revision/Rev.java
@@ -1,26 +1,60 @@
 // /* 
 //     Even/Odd
-// */
+// // */
+// public class Rev {
+
+//     public static void main(String [] args) {
+
+//         try {
+//             int num = 10;
+
+//             if(num % 2 == 0) {
+
+//                 System.out.println("Even Number");
+//             } else {
+
+//                 System.out.println("Odd Number");
+//             }
+
+//         } 
+//         catch(ArithmeticException ae) {
+
+//             System.out.println("Invalid Value entered");
+//         }
+//     }
+// }
+
+/*
+    Prime number
+*/
+
 public class Rev {
 
+    /**
+     * @param args
+     */
     public static void main(String [] args) {
 
-        try {
-            int num = 10;
+        int num = 10;
+        boolean isPrime = true;
 
-            if(num % 2 == 0) {
+        if(num <= 1) {
 
-                System.out.println("Even Number");
-            } else {
-
-                System.out.println("Odd Number");
-            }
-
+            isPrime = false;
         } 
-        catch(ArithmeticException ae) {
 
-            System.out.println("Invalid Value entered");
+        for(int i=2; i < num; i++ ) {
+            if(num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }  
+        if(isPrime) {
+            System.out.println("Prime Number");
+        }     
+        if(!isPrime) {
+
+            System.out.println("Not Prime Number");
         }
     }
 }
-
