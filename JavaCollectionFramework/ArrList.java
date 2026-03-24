fdc49159eccce3c933752b378aa92e3661f8fd8c
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-24 10:00:00 +0530

    ArrayList in java

--- JavaCollectionFramework/ArrList.java
+++ JavaCollectionFramework/ArrList.java
@@ -10,59 +10,59 @@ Create an ArrayList of student names:
 */
 
 
-public class ArrList {
+// public class ArrList {
 
-    public static void main(String [] args) {
+//     public static void main(String [] args) {
 
-        ArrayList<String> list = new ArrayList<>();
+//         ArrayList<String> list = new ArrayList<>();
 
-        // add()=================================
-        list.add("Aditya Yadav");
-        list.add("Akash Yadav"); 
-        list.add("Anil Singh");
-        list.add("Chandan Thakur");
-        list.add("Abhishek Yadav");
-        list.add("Daksh Chauhan");
-        list.add("Deepak Kumar");
+//         // add()=================================
+//         list.add("Aditya Yadav");
+//         list.add("Akash Yadav"); 
+//         list.add("Anil Singh");
+//         list.add("Chandan Thakur");
+//         list.add("Abhishek Yadav");
+//         list.add("Daksh Chauhan");
+//         list.add("Deepak Kumar");
 
-        // get()=================================
-            System.out.println(list.get(0));
-            System.out.println(list.get(1));
-            System.out.println(list.get(2));
-            System.out.println(list.get(3));
-            System.out.println(list.get(4));
-            System.out.println(list.get(5)); 
+//         // get()=================================
+//             System.out.println(list.get(0));
+//             System.out.println(list.get(1));
+//             System.out.println(list.get(2));
+//             System.out.println(list.get(3));
+//             System.out.println(list.get(4));
+//             System.out.println(list.get(5)); 
 
-        // remove ==================================
-        list.remove(2);
-        System.out.println(list);     
+//         // remove ==================================
+//         list.remove(2);
+//         System.out.println(list);     
 
-    // size () =========================================
-    System.out.println(list.size());
-    }
-}
+//     // size () =========================================
+//     System.out.println(list.size());
+//     }
+// }
 
-// 1️⃣ Create ArrayList of integers
-// 	•	Add elements
-// 	•	Print using loop
+// // 1️⃣ Create ArrayList of integers
+// // 	•	Add elements
+// // 	•	Print using loop
 
-public class ArrList {
+// public class ArrList {
 
-    public static void main(String [] args) {
+//     public static void main(String [] args) {
 
-        ArrayList <Integer> list = new ArrayList<>();
+//         ArrayList <Integer> list = new ArrayList<>();
 
-        list.add(10);
-        list.add(20);
-        list.add(30);
+//         list.add(10);
+//         list.add(20);
+//         list.add(30);
 
-        // loop
-        for(int i =0; i < list.size(); i++) {
+//         // loop
+//         for(int i =0; i < list.size(); i++) {
 
-            System.out.println(list.get(i));
-        }
-    }
-}
+//             System.out.println(list.get(i));
+//         }
+//     }
+// }
 
 /*
     2️⃣ Create LinkedList
