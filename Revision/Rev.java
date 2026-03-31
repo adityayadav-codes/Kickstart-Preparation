235b35bde74b53b696837068fe7ffa9600274dbf
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-31 23:27:19 +0530

    code

--- Revision/Rev.java
+++ Revision/Rev.java
@@ -171,23 +171,23 @@
 
 // import java.util.Scanner;
 
-public class Rev {
+// public class Rev {
 
-    public static void main(String [] args) {
-        Scanner sc = new Scanner(System.in);
+//     public static void main(String [] args) {
+//         Scanner sc = new Scanner(System.in);
        
-        System.out.print("Enter a String : ");
-        String str = sc.nextLine();
-        int count=0;
-        for(int i =0; i < str.length(); i++) {
+//         System.out.print("Enter a String : ");
+//         String str = sc.nextLine();
+//         int count=0;
+//         for(int i =0; i < str.length(); i++) {
 
-            if(str.charAt(i)== 'a' || str.charAt(i) == 'e' || str.charAt(i)=='i'|| str.charAt(i)=='o'|| str.charAt(i)=='u') {
+//             if(str.charAt(i)== 'a' || str.charAt(i) == 'e' || str.charAt(i)=='i'|| str.charAt(i)=='o'|| str.charAt(i)=='u') {
 
-                count++;
-            }
-        }
+//                 count++;
+//             }
+//         }
 
-        System.out.println(count);
+//         System.out.println(count);
 
-    }
-}
+//     }
+// }
