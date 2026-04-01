16b42eed46c048536fe6b4bcd61a9d4d12e7f695
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-01 23:37:01 +0530

    Linear Searching with staic & onject creation

--- DSA/ArrTrav.java
+++ DSA/ArrTrav.java
@@ -12,9 +12,16 @@
 //     }
 // }
 
+
 class Temo {
 
+    /* 
+        Linear Sewarching with static method------------------
+    */
+
     static void linearSearch(int [] arr, int key) {
+
+        System.out.println("Linear Searching via static method--------------------");
         boolean isFound = false;
         for(int i =0; i < arr.length; i++) {
 
@@ -31,12 +38,40 @@ class Temo {
             System.out.println("Not Found");
         }
     }
-}
-public class ArrTrav {
-    public static void main (String [] args) {
+    
+    /*
+        Linear Searching with object---------------
+    */
+    void linearSearch2(int [] arr, int key) {
 
-        int []arr = {10,20,30,40,50};
+        System.out.println("Linear Searching via object");
+
+        boolean isFound = false;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] == key) {
+                System.out.println("Found");
+                isFound = true;
+                break;
+            }
+        }
+
+        if(!isFound) {
+            System.out.println("Not Found");
+        }
+    }
+}
+
+public class ArrTrav {
+
+    public static void main(String [] args) {
+
+        Temo obj = new Temo();
+
+        int [] arr = {10,20,30,40,50};
         int key = 30;
-       Temo.linearSearch(arr,key);
+        Temo.linearSearch(arr,key);
+        obj.linearSearch2(arr, key);
     }
 }
