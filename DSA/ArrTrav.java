8c30d0853cb0f56eeff84bd148c69c86b4fe2151
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-01 23:06:49 +0530

    Linear Searching

--- DSA/ArrTrav.java
+++ DSA/ArrTrav.java
@@ -1,13 +1,42 @@
-public class ArrTrav {
+// public class ArrTrav {
 
-    public static void main(String [] args) {
+//     public static void main(String [] args) {
 
-        int [] arr = {10,20,30,40};
+//         int [] arr = {10,20,30,40};
 
-        //for-loop
+//         //for-loop
+//         for(int i =0; i < arr.length; i++) {
+
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+class Temo {
+
+    static void linearSearch(int [] arr, int key) {
+        boolean isFound = false;
         for(int i =0; i < arr.length; i++) {
 
-            System.out.println(arr[i]);
+            if(arr[i] == key) {
+
+                System.out.println("Found");
+                isFound = true;
+                break;
+            }
         }
+
+        if(!isFound) {
+
+            System.out.println("Not Found");
+        }
+    }
+}
+public class ArrTrav {
+    public static void main (String [] args) {
+
+        int []arr = {10,20,30,40,50};
+        int key = 30;
+       Temo.linearSearch(arr,key);
     }
 }
