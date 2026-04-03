516bc0cc7935b9c0178114049c3ca32161c9b9c0
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-03 18:00:19 +0530

    Refverse Array

--- DSA/Searching/LinBinSearching.java
+++ DSA/Searching/LinBinSearching.java
@@ -25,19 +25,46 @@ public class LinBinSearching {
             System.out.println("Not found!");
         }
     }
+
+
+    /*
+        Reverse array (without extra array)----------------------------
+    */
+   static void revArray(int [] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    while(start < end) {
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;  
+    }
+    System.out.println("Reverse Array : ");
+    for(int num : arr) System.out.println(num + " ");
+   }
+
     public static void main(String[] args) {
         
-        int array[] = new int[10];
+         int array[] = new int[4];
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the elements");
+
+        System.out.println("Enter the elements:");
 
         for(int i = 0; i < array.length; i++) {
-
             array[i] = sc.nextInt();
         }
-    System.out.print("Enter target element ");
-    int target = sc.nextInt();
 
-    linearSearch(array, target);
+        revArray(array);
+    
+    // System.out.print("Enter target element ");
+    // int target = sc.nextInt();
+
+    // linearSearch(array, target);
+
 }
+
 }
+   
