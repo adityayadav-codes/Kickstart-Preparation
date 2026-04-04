be35e82d3466825ae3376e5386315859b92a7324
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-04 23:08:50 +0530

    Second Largest element

--- DSA/Searching/BinarySearching.java
+++ DSA/Searching/BinarySearching.java
@@ -25,11 +25,33 @@ public class BinarySearching {
         System.out.println("Not Found!");
     }
     }
+
+    /*
+        Find second largest element-------------
+    */
+   static void secondLargest(int arr[]) {
+
+    int largest = arr[0];
+    int secondLargest = arr[0];
+
+    for(int i = 0; i < arr.length; i++) {
+        if(arr[i]>largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        if(arr[i] > secondLargest && arr[i] != largest) {
+
+            secondLargest = arr[i];
+        }
+    }
+    System.out.println("Second Largest Element : "+secondLargest);
+   }
     // main mthod-------------
     public static void main(String [] args) {
 
-        int arr[] = {10,20,30,40,50,60,70,80,90,100};
+        int arr[] = {10,20,100,30,40,50,60,70};
         int target = 80;
-        binarySearch(arr, target);
+        // binarySearch(arr, target);
+        secondLargest(arr);
     }
 }
