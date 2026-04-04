7b4aded3ec7ebb04718a6592a1737b9d0499d245
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-05 00:14:06 +0530

    Find missing number in array (basic)

--- DSA/Arrays/Demo.java
+++ DSA/Arrays/Demo.java
@@ -43,9 +43,23 @@ static void arraySorted(int arr[]) {
         System.out.println("not sorted");
     }
 }
+/*
+    Find missing number in array (basic)-----------------
+*/
+static void missingNumber(int arr[]) {
+    int n = 4;
+    int totalArray = n * (n + 1)/2;
+    int sumArray = 0;
+    for(int i = 0; i < arr.length; i ++) {
+        sumArray += arr[i];
+    }
+    int missingNumber = totalArray - sumArray;
+    System.out.println("Missing Number : "+missingNumber);
+}
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4};
+        int arr[] = {1,3,4};
         // moveZeroesToEnd(arr);
-        arraySorted(arr);
+        // arraySorted(arr);
+        missingNumber(arr);
     }
 }
