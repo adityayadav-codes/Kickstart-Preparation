160c473c00d893c98908e6796ace7f0284633ea1
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-04 23:36:30 +0530

    Move all zeros to the end of array

---
+++ DSA/Arrays/Demo.java
@@ -0,0 +1,26 @@
+/*
+    Move all zeros to end of array
+*/
+public class Demo{
+
+    static void moveZeroesToEnd(int arr[]) {
+
+        int j = 0;
+        for(int i = 0; i < arr.length; i++) {
+
+        if(arr[i] != 0) {       // i = 0 ❌   j =0              i = 1✅ j = 1      
+            int  temp = arr[i]; // 0 -> 1 then temp = 0      |  0 -> 2 then temp 
+            arr[i] = arr[j];    // arrOfi.0 -> 1 par 1       |
+            arr[j] = temp;       // arrofj.0 -> 0            |
+
+            j++;
+        }
+    } 
+    for(int num : arr) System.out.print(num + " ");
+    System.out.println();
+}   
+    public static void main(String[] args) {
+        int arr[] = {1,0,2,0,3,4};
+        moveZeroesToEnd(arr);
+    }
+}
