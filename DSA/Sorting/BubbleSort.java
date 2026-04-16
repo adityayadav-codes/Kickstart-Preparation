a5b3962fc7cfd92ac3b94455ac17970664c6cb8d
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-16 15:35:28 +0530

    Bubble Sorting

---
+++ DSA/Sorting/BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSort {
+    public static void main(String [] args) {
+
+        int []arr = {20,10,40,30};
+
+        for(int i = 0; i < arr.length - 1; i++) {
+
+            for(int j = 0 ; j < arr.length - 1; j++) {
+
+                if(arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+             }
+        }
+
+        for(int num : arr) System.out.print(num +" ");
+        System.out.println();
+
+    }
+}
