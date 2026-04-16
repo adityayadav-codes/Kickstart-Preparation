9d8d8985a4bc1231215e407d25bbcec27e1708d8
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-16 22:05:11 +0530

    Descending Order

--- DSA/Sorting/BubbleSort.java
+++ DSA/Sorting/BubbleSort.java
@@ -24,6 +24,22 @@
 
 public class BubbleSort {
 
+    static void bubbleSortWithDesc(int []arr) {
+
+        for(int i = 0; i < arr.length - 1; i++) {
+
+            for(int j = 0; j < arr.length - 1; j++) {
+
+                if(arr[j] < arr[j + 1]) {
+
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int num : arr) System.out.print(num + " ");
+    }
    static void bubbleSortWithAsc(int arr[]) {
         
          for(int i = 0; i < arr.length - 1; i++) {
@@ -43,9 +59,12 @@ public class BubbleSort {
     }
     public static void main(String[] args) {
     
-        int []arr = {20,10,40,30};
+        int []arr = {50,60,80,90,70,30};
 
+        System.out.println("Ascending Order : ");
         bubbleSortWithAsc(arr);
-
+        System.err.println();
+        System.out.println("Descending Order : ");
+        bubbleSortWithDesc(arr);
     }
 }  
