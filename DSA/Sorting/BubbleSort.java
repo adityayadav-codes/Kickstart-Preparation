31f706b4aaed70de8bc6545e26c2e16769f0cf17
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-16 20:58:36 +0530

    Bubble Sort with Ascending order

--- DSA/Sorting/BubbleSort.java
+++ DSA/Sorting/BubbleSort.java
@@ -1,11 +1,34 @@
+// public class BubbleSort {
+//     public static void main(String [] args) {
+
+//         int []arr = {20,10,40,30};
+
+//         for(int i = 0; i < arr.length - 1; i++) {
+
+//             for(int j = 0 ; j < arr.length - 1; j++) {
+
+//                 if(arr[j] > arr[j + 1]) {
+
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                 }
+//              }
+//         }
+
+//         for(int num : arr) System.out.print(num +" ");
+//         System.out.println();
+
+//     }
+// }
+
 public class BubbleSort {
-    public static void main(String [] args) {
 
-        int []arr = {20,10,40,30};
+   static void bubbleSortWithAsc(int arr[]) {
+        
+         for(int i = 0; i < arr.length - 1; i++) {
 
-        for(int i = 0; i < arr.length - 1; i++) {
-
-            for(int j = 0 ; j < arr.length - 1; j++) {
+            for(int j = 0; j < arr.length - 1; j++) {
 
                 if(arr[j] > arr[j + 1]) {
 
@@ -13,11 +36,16 @@ public class BubbleSort {
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
-             }
+            }
         }
 
-        for(int num : arr) System.out.print(num +" ");
-        System.out.println();
+        for(int num : arr) System.out.print(num + " ");
+    }
+    public static void main(String[] args) {
+    
+        int []arr = {20,10,40,30};
+
+        bubbleSortWithAsc(arr);
 
     }
-}
+}  
