096c13afd1f8599c1dd39b9edadea39b22378d27
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-18 00:12:43 +0530

    find duplicates element

--- DSA/Hasing/HashOper2.java
+++ DSA/Hasing/HashOper2.java
@@ -47,11 +47,45 @@ public class HashOper2 {
             
         }
     }
- public static void main(String[] args) {
+    //----------------------------------------------------------------------
+    static void findDuplicateElements(int []arr) {
+
+        HashMap <Integer, Integer> map = new HashMap<>();
+
+        System.out.print("Duplicates Element : ");
+        
+
+            // if(map.containsKey(num)) {
+
+            //     System.out.print(num + " ");
+            // }
+            // else {
+
+            //     map.put(num,1);
+            // }
+        for(int num : arr) {
+
+            if(map.containsKey(num)) {
+                map.put(num, map.get(num)+1);
+            } else {
+                map.put(num, 1);
+            } 
+        }
+        for(int key : map.keySet()) {
+
+                if(map.get(key) > 1) {
+
+                    System.out.print(key+" ");
+                }
+            }
+    }
+    public static void main(String[] args) {
         // int []arr = {1,2,3,2,1,4};
-        int [] arr = {1,2,2,3};
+        int [] arr = {1,2,2,3,3};
 
         // firstNonRepElem(arr);
-        countFrequencyUsingMap(arr);
+        // countFrequencyUsingMap(arr);
+        findDuplicateElements(arr);
+        
     }
 }
