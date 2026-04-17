9b4ea8ddb2846e2dce4151fdc81b100b633171bd
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-17 23:29:28 +0530

    Count Frequency Using HashMap

--- DSA/Hasing/HashOper2.java
+++ DSA/Hasing/HashOper2.java
@@ -1,4 +1,7 @@
+import java.lang.classfile.instruction.StackInstruction;
 import java.util.HashMap;
+
+import javax.xml.transform.stax.StAXResult;
 
 public class HashOper2 {
 
@@ -20,9 +23,35 @@ public class HashOper2 {
 
         for(int num : arr) if(map.get(num)==1) {System.out.println("First not Repeating element is "+num); break;}
     } 
-    public static void main(String[] args) {
-        int []arr = {1,2,3,2,1,4};
+   
+ //   --------------------------------------------------------------
+    
+        static void countFrequencyUsingMap(int []arr) {
 
-        firstNonRepElem(arr);
+            HashMap<Integer,Integer> map = new HashMap<>();
+
+            for(int num : arr) {
+
+                if(map.containsKey(num)) {
+
+                    map.put(num, map.get(num)+1);
+                }
+                 else {
+
+                map.put(num, 1);
+            }
+        }
+        for(int key : map.keySet()) {
+
+            System.out.println(key + " --> " +map.get(key));
+            
+        }
+    }
+ public static void main(String[] args) {
+        // int []arr = {1,2,3,2,1,4};
+        int [] arr = {1,2,2,3};
+
+        // firstNonRepElem(arr);
+        countFrequencyUsingMap(arr);
     }
 }
